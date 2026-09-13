b93539fd36a46e62082f992364f7866e1ce139f6
Icemer <ismalo1404@gmail.com>
2026-09-12 17:11:39 -0700

    Base para mostrar Posts

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use App\Models\Post;
+use App\Models\Category;
+
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -15,11 +18,25 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
+        User::create([
             'name' => 'Test User',
-            'email' => 'test@example.com',
+            'email' => 'Test@example.com',
+            'password' => 'TestPasswd',
         ]);
+
+        $categories = [
+            'Technology',
+            'Health',
+            'Science',
+            'Sports',
+            'Politics',
+            'Entertainment',
+        ];
+
+        foreach ($categories as $category) {
+            Category::create(['name' => $category]);
+        }
+
+        Post::factory(100)->create();
     }
 }
