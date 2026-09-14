f08ce2577390eceed4559c0cb9ee60e3f4ba1f8a
Icemer <ismalo1404@gmail.com>
2026-09-14 11:03:38 -0700

    Create Post Version 1

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -26,7 +26,10 @@ class PostController extends Controller
      */
     public function create()
     {
-        //
+        $categories = Category::get();
+        return view('post.create',[
+            'categories'=>$categories,
+        ]);
     }
 
     /**
@@ -34,7 +37,12 @@ class PostController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $data = $request->validate([
+            'image'=> ['required','image','mimes:jpeg,png,jpg,gif,svg','max:2048'],
+            'title'=> 'required',
+            'content'=> 'required',
+            'category_id' => 'required| exists:categories,id'
+        ]);
     }
 
     /**
