fdc7b64566ef4c891110f8d79d374e3035dc1b15
Icemer <ismalo1404@gmail.com>
2026-09-17 13:58:23 -0700

    Implement Follow/Unfollow version 1

--- resources/views/profile/show.blade.php
+++ resources/views/profile/show.blade.php
@@ -14,18 +14,22 @@
                             @endforelse
                         </div>
                     </div>
-                    <div class="w-[320px] border-l px-8">
+                    <div x-data="{
+                        following: {{ $user->isFollowedBy(auth()->user()) ? 'true' : 'false'}},
+                    }"class="w-[320px] border-l px-8">
                         <x-user-avatar :user='$user' size="w-24 h-24"/>
                         <h3>{{ $user->name }}</h3>
-                        <p class="text-gray-500">26k followers</p>
+                        <p class="text-gray-500">{{ $user->followers()->count() }} followers</p>
                         <p>
                             {{ $user->bio }}
                         </p>
-                        <div class="mt-4">
-                            <button class="bg-emerald-600 rounded-full px-4 py-2 text-white">
-                                Follow
-                            </button>
-                        </div>
+                        @if (auth()->user() && auth()->user()->id !== $user->id)
+                            <div class="mt-4">
+                                <button x-text="following ? 'Unfollow' : 'Follow'" class="bg-emerald-600 rounded-full px-4 py-2 text-white">
+                                    Follow
+                                </button>
+                            </div>
+                        @endif
                     </div>
                 </div>
             </div>
